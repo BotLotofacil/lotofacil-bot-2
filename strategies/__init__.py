20d34226e12407936444eca5497bcc8a23728268
BotLotofacil <lotofacil.ai.2025@gmail.com>
2025-10-21 22:08:43 -0300

    Add HybridGenerator to strategies module

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -2,5 +2,6 @@
 from .genetic import GeneticStrategy
 from .freq_strategy import FreqStrategy
 from .neural import NeuralStrategy
+from .hybrid import HybridGenerator  # <-- ADICIONE
 
 __all__ = ["GeneticStrategy", "FreqStrategy", "NeuralStrategy"]
